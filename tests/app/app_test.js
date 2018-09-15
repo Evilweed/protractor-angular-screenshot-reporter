162f20b2eb4caa1984cd715be54f27fcc870359a
miller45 <gmse45forse@googlemail.com>
2018-09-15 18:49:36 +0200

    adjust tests to different timezones

--- tests/app/app_test.js
+++ tests/app/app_test.js
@@ -90,11 +90,14 @@ describe('unit tests', function () {
                 it('convertTimestamp handles all cases', function () {
                     var $scope = $rootScope.$new();
                     var controller = $controller('ScreenshotReportController', {$scope: $scope});
-
-                    expect(controller.convertTimestamp(1534696710055)).toEqual("2018-08-19, 6:38 PM");
-                    expect(controller.convertTimestamp(1534396700055)).toEqual("2018-08-16, 7:18 AM");
-                    expect(controller.convertTimestamp(1543618800000)).toEqual("2018-12-01, 12:00 AM");
-                    expect(controller.convertTimestamp(1543662000000)).toEqual("2018-12-01, 12:00 PM");
+                    var offset = new Date().getTimezoneOffset();
+                    expect(timeData).toBeDefined();
+                    var refSet = timeData[offset];
+                    expect(refSet).toBeDefined("No reference data for timezoneOffset "+offset);
+                    expect(controller.convertTimestamp(1534696710055)).toEqual(refSet["1534696710055"]); // 2018-08-19, 6:38 PM
+                    expect(controller.convertTimestamp(1534396700055)).toEqual(refSet["1534396700055"]); // e.g. "2018-08-16, 7:18 AM"
+                    expect(controller.convertTimestamp(1543618800000)).toEqual(refSet["1543618800000"]); //e.g. "2018-12-01, 12:00 AM"
+                    expect(controller.convertTimestamp(1543662000000)).toEqual(refSet["1543662000000"]);  // e.g. "2018-12-01, 12:00 PM");
 
                 });
 
@@ -431,8 +434,8 @@ describe('unit tests', function () {
             });
 
             it('testDataBackup is ok', function () {
-               expect(testDataBackup).toBeDefined();
-               expect(testDataBackup.length).toEqual(12);
+                expect(testDataBackup).toBeDefined();
+                expect(testDataBackup.length).toEqual(12);
             });
 
             it('when test data is empty', function () {
